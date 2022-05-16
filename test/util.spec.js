011f355c135eed3b7e0d0c90a6b53ef6e3c2116a
Nay Trevejo <nay.trevejo@gmail.com>
2022-05-16 16:51:49 -0500

    test env fixed

--- test/util.spec.js
+++ test/util.spec.js
@@ -1,4 +1,4 @@
-import { getAbsolutePath } from '../util.js'
+import { getAbsolutePath, fileExists, readFile } from '../util.js'
 
 const routeTest = 'C:\\Users\\Andrea Trevejo\\Desktop\\Laboratoria\\LIM017-md-links\\MD3.md'
 
@@ -6,4 +6,17 @@ describe('getAbsolutePath', () => {
   it('Show resolve path in windows', () => {
     expect(getAbsolutePath('./MD3.md')).toBe(routeTest)
   })
-})
+});
+
+describe ('fileExists', () => {
+    it('show if the file of the route exists', () => {
+        expect(fileExists('./MD3.md')).toBe(true);
+    })
+});
+
+describe('readFile', () => {
+    it('shows the content of the file',() =>{
+        expect(readFile('./MD3.md')).toBe('pruebita pruebota')
+    })
+});
+
--- util.js
+++ util.js
@@ -1,3 +1,6 @@
 import path from 'path'
+import fs from 'fs'
 
 export const getAbsolutePath = (route) => path.isAbsolute(route) ? route : path.resolve(route);
+export const fileExists = (route) => fs.existsSync(route); //booleano
+export const readFile = (route) => fs.readFileSync(route, 'utf-8') // contenido del archivo
