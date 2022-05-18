df4a6b670295dfc16394f9fc3cc8d867a5ce9771
Nay Trevejo <nay.trevejo@gmail.com>
2022-05-18 17:23:27 -0500

    before rfx

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-import path from 'path'
-import process from 'process'
-import * as fs from 'fs'
-import { getAbsolutePath, fileExists, readFile } from './util.js'
-
+import process from 'process';
+import { getAbsolutePath, fileExists, readFile } from './util.js';
 
 const mdLinks = (route, options) => {
   if (!route) {
-    console.log('no hay path')
-    return
+    console.log('no hay path');
+    return;
   }
   console.log(getAbsolutePath(route));
-  if (!fileExists){
-    console.log('The file does not exist')
-    return
+  if (!fileExists) {
+    console.log('The file does not exist');
+    return;
   }
   console.log(readFile);
-}
+};
 
 //  Agarra segundo argumento de la terminal
-mdLinks(process.argv[2])
-
+mdLinks(process.argv[2]);
 
 // module.exports = () => {
 //   // ...
--- util.js
+++ util.js
@@ -1,9 +1,9 @@
-import path from 'path'
-import fs from 'fs'
+import path from 'path';
+import fs from 'fs';
 
 export const getAbsolutePath = (route) => path.isAbsolute(route) ? route : path.resolve(route);
-export const fileExists = (route) => fs.existsSync(route); //booleano
-export const readFile = (route) => fs.readFileSync(route, 'utf-8') // contenido del archivo
+export const fileExists = (route) => fs.existsSync(route); // booleano
+export const readFile = (route) => fs.readFileSync(route, 'utf-8'); // contenido del archivo
 
 // no se guarda el valor porque no termina la promesa
 // export const readFile = async (route) => await fs.readFile(route, (err, data) => {
