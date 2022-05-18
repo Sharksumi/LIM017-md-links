2706c19c7e51a24671f88915bcab3f4f989ba460
Nay Trevejo <nay.trevejo@gmail.com>
2022-05-18 18:27:33 -0500

    read directories done

--- util.js
+++ util.js
@@ -5,9 +5,35 @@ export const getAbsolutePath = (route) => path.isAbsolute(route) ? route : path.
 export const fileExists = (route) => fs.existsSync(route); // booleano
 export const readFile = (route) => fs.readFileSync(route, 'utf-8'); // contenido del archivo
 
-// no se guarda el valor porque no termina la promesa
-// export const readFile = async (route) => await fs.readFile(route, (err, data) => {
-//     if (err) return console.error(err);
-//     console.log(data.toString());
-//     return data.toString();
-// });
+const linksList = [];
+
+const routeTest2 = 'C:\\Users\\Andrea Trevejo\\Desktop\\Laboratoria\\LIM017-md-links\\example';
+
+const goToDirectory = (route) => {
+  const dirList = fs.readdirSync(route);
+  console.log(dirList);
+  dirList.forEach(dir => {
+    const concatPath = path.join(route, dir);
+    if (fs.statSync(concatPath).isFile()) {
+      console.log('Esta ruta es archivo', concatPath);
+      if (path.extname(concatPath) === '.md') { // para saber si es archivo md
+        linksList.push(concatPath);
+      }
+    } else {
+      goToDirectory(concatPath);
+    }
+  });
+};
+
+goToDirectory(routeTest2);
+console.log(linksList);
+
+// function countDown (fromNumber) {
+//   console.log(fromNumber);
+
+//   const nextNumber = fromNumber - 1;
+
+//   if (nextNumber > 0) {
+//     countDown(nextNumber);
+//   }
+// }
